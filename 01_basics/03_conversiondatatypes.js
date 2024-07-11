9baa3b91cee455622f55278992c8aaa7ffae5427
sahilkumar2002 <154125748+sahilkumar2002@users.noreply.github.com>
2024-07-11 19:19:13 +0000

    conversion of datatypes

--- 01_basics/03_conversiondatatypes.js
+++ 01_basics/03_conversiondatatypes.js
@@ -21,5 +21,45 @@ let bolleanIsLoggedIn =Boolean(isLoggedIn);
 
 let someNumber=44
 let stringNumber=String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//***************operation*************/
+let value=3
+let negValue=-value
+//console.log(negValue);//-3
+//console.log(3+3);
+// console.log(3*3);//9
+// console.log(3-3);//0
+// console.log(2**3);//8
+// console.log(2/3);//0.6666
+// console.log(2%3);//2
+ let str1="Hello"
+ let str2=" sir"
+ let str3=str1 + str2
+//  console.log(str3)
+
+//special type conversion
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+// console.log((3 +4)* 5%3);//2
+
+// console.log(true);//true
+// console.log(+true);//1
+// console.log(true+);//Error
+// console.log(+"")//false
+// let x=100;
+// console.log(x++);//100
+// console.log(++x);//102
+ let num1, num2,num3
+ num1= num2= num3=2+2
+
+ let Gamecounter=100;
+ ++Gamecounter
+ console.log(Gamecounter);//101
+  //link to study 
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
