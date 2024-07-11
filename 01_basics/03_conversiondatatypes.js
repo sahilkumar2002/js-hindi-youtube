3ba8128ff2d64a1fa0d54b7afa5a930311349c81
sahilkumar2002 <154125748+sahilkumar2002@users.noreply.github.com>
2024-07-11 18:43:08 +0000

    conversion Of Datatypes

---
+++ 01_basics/03_conversiondatatypes.js
@@ -0,0 +1,25 @@
+let score ="sahil"
+//console.log(typeof score);//string
+//console.log(typeof(score));//string
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);//Nan not a number
+//console.log(typeof valueInNumber)//Number
+
+// "33" => 33
+//"33abc" => Nan
+//true =>1 ; false=> 0
+
+let isLoggedIn =""
+let bolleanIsLoggedIn =Boolean(isLoggedIn);
+//console.log(bolleanIsLoggedIn)
+//console.log(typeof bolleanIsLoggedIn)
+
+//1=>true; 0=>false
+//"" =>false
+//"sahil"=>true
+
+let someNumber=44
+let stringNumber=String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
