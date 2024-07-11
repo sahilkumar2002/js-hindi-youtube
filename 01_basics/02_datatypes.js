adc16e88b7192248152fce4a4c8ed353742e4aef
sahilkumar2002 <154125748+sahilkumar2002@users.noreply.github.com>
2024-07-11 17:46:48 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //we are using nodejs, not browser
+console.log(3
+    +3
+)//code readabiltiy should be high
+
+console.log("sahil")
+
+let name="sahil"
+let age=21
+let isLoggedIn=false
+let state=null;
+let user;
+//number =>2 to power 53
+//bigInt
+//string=> ""
+//boolean -> true/false
+//null=>standalone value
+//undefine=>
+//symbol =>unique for use symbol
+
+//object 
+console.log(typeof"sahil")//string
+console.log(typeof age)//number
+console.log(typeof state)//object 
+console.log(typeof user)//undefined
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object 
