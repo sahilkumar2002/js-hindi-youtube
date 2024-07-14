411a4ef44d14317fe12ee491b258613770c51021
sahilkumar2002 <154125748+sahilkumar2002@users.noreply.github.com>
2024-07-14 18:59:23 +0000

    04_objects

---
+++ 02_basics/04_object.js
@@ -0,0 +1,49 @@
+// const tinderuser= new Object()//{}
+//singleton object 
+const tinderuser={}//{}
+//nonsingleton object
+tinderuser.id="abc123"
+tinderuser.name="sahilkumar"
+tinderuser.isLoggedIn=false
+// console.log(tinderuser);
+
+const regularuser={
+    email:"sahdfh@hgian.com",
+    fullname:{
+        userfullanme:{
+            firstname:"sahil",
+            lastname:"kumar"
+        }
+    
+    }
+}
+// console.log(regularuser.fullname.userfullanme.lastname);
+
+const obj1={
+    1:"a",
+    2:"b"
+}
+const obj2={
+      2:"c",
+      3:"d"
+}
+
+// const obj3=Object.assign({},obj1,obj2);
+// console.log(obj3);//  target ,source
+                    //object   object
+//The Object.assign() static method copies all enumerable own properties from one 
+//or more source objects to a target object. It returns the modified target object.
+// const obj3={...obj1,...obj2};
+// console.log(obj3);
+// // { '1': 'a', '2': 'c', '3': 'd' }
+
+
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser))
+
+console.log(tinderuser.hasOwnProperty('isLoggedIn'));
+//proeperty exist or not
+
+
